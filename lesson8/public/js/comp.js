ef8a8a53ac8c45cc38254849a755a5f94c4c31a3
Galand33rus <galand_@mail.ru>
2021-08-20 13:16:26 +0300

    lesson8

---
+++ lesson8/public/js/comp.js
@@ -0,0 +1,15 @@
+
+import CartComp from './cart-comp';
+
+
+import ProductGridComp from './product-comp';
+
+
+import Error from './error';
+
+export default {
+    components:
+    CartComp,
+    ProductComp,
+    Error
+}
